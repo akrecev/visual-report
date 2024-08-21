f234f74159f8c41e09299f81b931a29a212c38f7
askretsev <kretsev@yandex.ru>
2024-08-21 09:40:15 +0300

    fix ReportController

--- src/main/java/com/rokdc/report/controller/ReportController.java
+++ src/main/java/com/rokdc/report/controller/ReportController.java
@@ -1,56 +1,36 @@
 package com.rokdc.report.controller;
 
 import com.rokdc.report.exception.PropertiesLoadException;
-import com.rokdc.report.log.LogWriter;
 import com.rokdc.report.model.App;
 import com.rokdc.report.service.PageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ReportController {
     private final String mainPage;
-
     private final PageService pageService;
 
-    public ReportController(@Autowired PageService pageService
-            , @Autowired LogWriter log
-            , @Value("${settings.start-page}") String mainPage) {
+    public ReportController(@Autowired PageService pageService, @Value("${settings.start-page}") String mainPage) {
         this.pageService = pageService;
         this.mainPage = mainPage;
     }
 
     @GetMapping(value = "/")
-    public String index(Model model) throws PropertiesLoadException {
+    public String index() throws PropertiesLoadException {
         App app = pageService.getApp();
-
         if (app != null) {
             return mainPage;
-
         } else
             throw new PropertiesLoadException(getClass() + ": fail to load index page. Add index page to your properties.json");
     }
 
-
-    @GetMapping(value = "/{name}")
-    public String index(Model model, @PathVariable("name") String name) throws PropertiesLoadException {
-        App app = pageService.getApp();
-
-        if (app != null) {
-            return mainPage;
-
-        } else
-            throw new PropertiesLoadException(getClass() + ": fail to load index page. Add index page to your properties.json");
-    }
-
-    @GetMapping(value = "500")
-    public ModelAndView _500(Model model) {
+    @GetMapping(value = "/500")
+    public ModelAndView serverError() {
         return new ModelAndView("500", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
